21d7e45ba4b4264c6106c357d916dcb89b00a050
edz <wangqi@itcyx.cn>
2020-08-17 16:29:05 +0800

    用Math.min 去中毒间隔最小的值

--- keepAlgorithm/src/main/java/com/leecode/array/visit/leecode_495.java
+++ keepAlgorithm/src/main/java/com/leecode/array/visit/leecode_495.java
@@ -1,8 +1,11 @@
 package com.leecode.array.visit;
 
+import java.util.ArrayList;
+
 /**
  * 495. 提莫攻击 难度：中等
- * 在《英雄联盟》的世界中，有一个叫 “提莫” 的英雄，他的攻击可以让敌方英雄艾希（编者注：寒冰射手）进入中毒状态。现在，给出提莫对艾希的攻击时间序列和提莫攻击的中毒持续时间，你需要输出艾希的中毒状态总时长。
+ * 在《英雄联盟》的世界中，有一个叫 “提莫” 的英雄，他的攻击可以让敌方英雄艾希（编者注：寒冰射手）进入中毒状态。
+ * 现在，给出提莫对艾希的攻击时间序列和提莫攻击的中毒持续时间，你需要输出艾希的中毒状态总时长。
  *
  * 你可以认为提莫在给定的时间点进行攻击，并立即使艾希处于中毒状态。
  * 示例1:
@@ -28,6 +31,19 @@ package com.leecode.array.visit;
  */
 public class leecode_495 {
     public static void main(String[] args) {
+        int[] arr = {1,4,6,7};
+        int inv = 3;
+        int attack = funcOne(arr,inv);
+        System.out.println(attack);
+    }
 
+    private static int funcOne(int[] arr,int inv){
+        int total = 0;
+        for (int i=0;i<arr.length-1;i++){
+            int during = Math.min(arr[i+1]-arr[i],inv);
+            total+=during;
+        }
+        total+=inv;
+        return total;
     }
 }
