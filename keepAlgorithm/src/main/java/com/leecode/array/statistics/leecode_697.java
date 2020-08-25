977461a6d7b4098b6c5d71dea7e778b35cd3ae31
edz <wangqi@itcyx.cn>
2020-08-25 09:55:46 +0800

    刷一题

--- keepAlgorithm/src/main/java/com/leecode/array/statistics/leecode_697.java
+++ keepAlgorithm/src/main/java/com/leecode/array/statistics/leecode_697.java
@@ -1,5 +1,10 @@
 package com.leecode.array.statistics;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 697. 数组的度 难度：简单
  * 给定一个非空且只包含非负数的整数数组 nums, 数组的度的定义是指数组里任一元素出现频数的最大值。
@@ -26,6 +31,37 @@ package com.leecode.array.statistics;
  */
 public class leecode_697 {
     public static void main(String[] args) {
+//        int[] arr = {1,2,2,3,1,4,2};
+        int[] arr = {1, 2, 2, 3, 1};
+        int min = funcOne(arr);
+        System.out.println(min);
+    }
+    private static int funcOne(int[] arr){
+        Map<Integer,ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+        for (int i=0;i<arr.length ; i++){
+            if (map.get(arr[i]) == null){
+                ArrayList<Integer> rst = new ArrayList<Integer>();
+                rst.add(i);
+                rst.add(50000);
+                rst.add(1);
+                map.put(arr[i],rst);
+            }else{
+                map.get(arr[i]).set(1,i);
+                map.get(arr[i]).set(2,map.get(arr[i]).get(2)+1);
+            }
+            System.out.println(map);
+        }
+        int min = 50000;
+        int degree = -1;
+        for(ArrayList<Integer> value: map.values()){
+            degree = Math.max(degree,value.get(2));
+        }
 
+        for (Integer key: map.keySet()){
+            if (degree == map.get(key).get(2)){
+                min = Math.min(min,map.get(key).get(1) -map.get(key).get(0)+1);
+            }
+        }
+        return min;
     }
 }
---
+++ keepAlgorithm/src/main/java/com/leecode/array/twovector/leecode_119.java
@@ -0,0 +1,22 @@
+package com.leecode.array.twovector;
+/**
+ *119. 杨辉三角 II
+ * 难度:简单
+ * 给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行。
+
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ *
+ * 示例:
+ *
+ * 输入: 3
+ * 输出: [1,3,3,1]
+ * 进阶：
+ *
+ * 你可以优化你的算法到 O(k) 空间复杂度吗？
+ */
+public class leecode_119 {
+
+    public static void main(String[] args) {
+
+    }
+}
